ed6d93091c212261367d37b632113ff1ff300b40
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-09-19 17:21:04 -0400

    [REF]: python service refactored

--- microservices/product-save-service/main.py
+++ microservices/product-save-service/main.py
@@ -1,50 +1,12 @@
-from app import app, mongo
-from bson.json_util import dumps
-from bson.objectid import ObjectId
-from flask import jsonify, flash, request
+from flask import Flask
 
-@app.route('/product/add', methods=['POST'])
-def add_user():
-	_json = request.json
-	_name = _json['productName']
-	_image = _json['image']
-	_from = _json['from']
-	_nutrients = _json['nutrients']
-	_quantity = _json['quantity']
-	_price = _json['price']
-	_organic = _json['organic']
-	_description = _json['description']
+app = Flask(__name__)
 
-	if request.method == 'POST':
-		id = mongo.db.product.insert(
-            {
-                'productName': _name, 
-                'image': _image, 
-                'from': _from, 
-                'quantity': _quantity, 
-                'nutrients': _nutrients,
-                'quantity': _quantity,
-                'price': _price,
-                'organic': _organic,
-                'description': _description
-            }
-        )
-		resp = jsonify('Product added successfully!')
-		resp.status_code = 200
-		return resp
-	else:
-		return not_found()
-				
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-        'status': 404,
-        'message': 'Not Found: ' + request.url,
-    }
-    resp = jsonify(message)
-    resp.status_code = 404
 
-    return resp
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__ == '__main__':
+    app.run(debug=True)
